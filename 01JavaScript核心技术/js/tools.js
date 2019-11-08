6f566b0c5123f2c44a3571485f33f9284315ff9c
strangersj <strangersj@outlook.com>
2019-11-08 23:39:56 +0800

    工具函数

--- 01JavaScript核心技术/js/tools.js
+++ 01JavaScript核心技术/js/tools.js
@@ -58,3 +58,42 @@ function move(obj, attr, target, speed, callback) {
 
     }, 30);
 }
+
+// class类的操作
+// 定义一个函数，用来向一个元素中添加指定的class属性值
+// 参数：obj，要添加class属性的元素；cn，要添加的class类名
+function addClass(obj, cn) {
+    if (!hasClass(obj, cn)) {
+        obj.className += " " + cn;
+    }
+}
+
+// 定义一个函数，用来从一个元素中删除指定的class属性值
+// 参数：obj，要删除class属性值的元素；cn，要删除的class类名
+function removeClass(obj, cn) {
+    let reg = new RegExp("\\b" + cn + "\\b");
+    // 删除响应class及将其替换为""
+    obj.className = obj.className.replace(reg, "");
+}
+
+// 定义一个函数，判断一个元素中是否含有指定的class属性
+// 参数：obj，要判断class属性的元素；cn，要判断的class类名
+// 如果有的返回true，没有则返回false
+function hasClass(obj, cn) {
+    let reg = new RegExp("\\b" + cn + "\\b"); // \b匹配单词边界
+
+    return reg.test(obj.className);
+}
+
+// 切换class，如果元素中具有该类则删除，如果元素中没有该类则添加
+// 
+function toggleClass(obj, cn) {
+    // 判断时候有
+    if (hasClass(obj, cn)) {
+        // 有则删除
+        removeClass(obj, cn);
+    } else {
+        // 没有则添加
+        addClass(obj, cn);
+    }
+}
