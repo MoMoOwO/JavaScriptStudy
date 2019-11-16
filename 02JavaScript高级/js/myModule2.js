134ff9a17060d8b28a77c53bb1c6ce98346ce9ed
strangersj <strangersj@outlook.com>
2019-11-16 22:53:53 +0800

    闭包的应用_自定义JS模块

---
+++ 02JavaScript高级/js/myModule2.js
@@ -0,0 +1,16 @@
+(function (window) {
+    // 私有数据，外部不能访问
+    var msg = "A Message!"; 
+    // 操作数据的函数
+    function doSomething() {
+        console.log("doSomething() " + msg.toUpperCase());
+    }
+    function doOtherthing() {
+        console.log("doOtherthing() " + msg.toLowerCase());
+    }
+    // 向外暴漏对象(给外部使用的方法)
+    window.myModule2 = {
+        doSomething: doSomething,
+        doOtherthing: doOtherthing
+    };
+})(window);
