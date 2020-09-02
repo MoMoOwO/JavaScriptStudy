a6888265fb7e837eaeff508b39a14ecbc1a9bf89
strangersj <strangersj@outlook.com>
2020-09-02 23:16:45 +0800

    双向链表 insert 方法

--- 05JS数据结构与算法/线性结构/07.双向链表结构.js
+++ 05JS数据结构与算法/线性结构/07.双向链表结构.js
@@ -65,6 +65,57 @@ function DoublyLinkedList() {
 
     return resultString;
   }
+
+  // 3. insert 将元素插入对应位置
+  DoublyLinkedList.prototype.insert = function (position, data) {
+    // 1. 越界判断
+    if (position < -1 || position > this.length) return false;
+
+    // 2. 根据 data 创建新节点
+    let newNode = new Node(data);
+
+    // 3. 判断插入位置
+    if (this.length === 0) { // 源列表为空，直接插入
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // 判断插入位置
+      if (position === 0) { // 插入到第一个位置
+        this.head.prev = newNode;
+        newNode.net = this.head;
+        this.head = newNode;
+      } else if (position === this.length) { // 插入到最后一个位置
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+      } else { // 插入任意中间位置
+        let current = null;
+        let index = 0;
+        // 寻找插入位置
+        if (position < this.length / 2) { // 插入节点在前半段，从前往后找
+          current = this.head;
+          while (index++ < position) {
+            current = current.next;
+          }
+        } else { // 插入节点在后半段，从后往前找
+          current = this.tail;
+          while (index++ < this.length - position - 1) {
+            current = current.prev;
+          }
+        }
+
+        // 插入元素
+        current.prev.next = newNode;
+        newNode.prev = current.prev;
+        newNode.next = current;
+        current.prev = newNode;
+      }
+    }
+
+    // 长度 +1
+    this.length += 1;
+    return true;
+  }
 }
 
 // 测试代码
@@ -78,3 +129,8 @@ list.append('bca');
 
 console.log(list.toString());
 console.log(list.forwardString());
+
+list.insert(2, 'ccc');
+console.log(list.toString());;
+list.insert(4, 'bbb');
+console.log(list.toString());
